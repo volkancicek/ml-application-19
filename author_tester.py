12e4f64f889d902203e276e044aec82ff171f89f
sony-Bilgisayar\sony <alkacd@gmail.com>
2019-05-15 00:26:25 +0200

    Created author_generator and tester

---
+++ author_tester.py
@@ -0,0 +1,14 @@
+from author_generator import *
+
+authors_data = [["Ahmet Emin","Mumtaz"], ["Elif","Taylan"], ["Dincer", "Faylan"], ["Dincero", "Faylana"], ["Duygu", "Gaylan"], ["Derin", "Geylan"], ["Duru", "Faylan"], ["Elif", "Faylana"]]
+
+a = AuthorGenerator()
+t = TitleGenerator()
+
+formatted_authors=[]
+for author_data in authors_data:
+    author = a.get_author_with_lastname_commablank_abbreviated_firstname_dot(author_data[0], author_data[1])
+    formatted_authors.append(author)
+
+formatted = a.get_author_names_in_apa_format(formatted_authors)
+print(formatted)
