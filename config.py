be8f1c008d69b1f7193a2d96d782bdd75fc071c2
volkancicek <volkanericlice@gmail.com>
2019-05-23 01:19:22 +0200

    [NO-ISSUE] added config

---
+++ config.py
@@ -0,0 +1,8 @@
+conf = {'input_file': "sample.json",
+        'output_file_json': 'reference_strings.json',
+        'output_file_txt': 'reference_strings.txt',
+        'output_file_csv': 'reference_strings.csv',
+        'reference_paper_count': 5,
+        'reference_styles': ('apa', 'chicago-annotated-bibliography', 'ieee'),
+        'max_reference_per_paper': 3
+        }
