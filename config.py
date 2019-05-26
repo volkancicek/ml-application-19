de83f01ef73106b9e2f1244229d3a16ea8400d7a
sony-Bilgisayar\sony <alkacd@gmail.com>
2019-05-27 01:16:08 +0200

    [NO-ISSUE] added more styles

--- config.py
+++ config.py
@@ -2,8 +2,13 @@ conf = {'input_file': "sample.json",
         'output_file_json': 'reference_strings.json',
         'output_file_txt': 'reference_strings.txt',
         'output_file_csv': 'reference_strings.csv',
-        'reference_paper_count': 50,
-        'reference_styles': ('apa', 'chicago-annotated-bibliography', 'ieee'),
+        'reference_paper_count': 10,
+        'reference_styles': (
+        'apa', 'chicago-annotated-bibliography', 'ieee', 'council-of-science-editors', 'iso690-author-date-en',
+        'modern-humanities-research-association', 'modern-language-association',
+        'american-society-of-mechanical-engineers', 'american-medical-association',
+        'american-chemical-society-author-date', 'american-sociological-association', 'turabian-fullnote-bibliography',
+        'acm-sig-proceedings', 'mcgill-en'),
         'max_reference_per_paper': 3,
         'cermine_result': 'cermine_result.txt'
         }
