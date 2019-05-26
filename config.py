c78811c762e24f13455024c7426a776fb109e2db
volkancicek <volkanericlice@gmail.com>
2019-05-26 12:38:06 +0200

    [NO-ISSUE] formatted

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ conf = {'input_file': "sample.json",
         'output_file_json': 'reference_strings.json',
         'output_file_txt': 'reference_strings.txt',
         'output_file_csv': 'reference_strings.csv',
-        'reference_paper_count': 5,
+        'reference_paper_count': 50,
         'reference_styles': ('apa', 'chicago-annotated-bibliography', 'ieee'),
         'max_reference_per_paper': 3,
         'cermine_result': 'cermine_result.txt'
