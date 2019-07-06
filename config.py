b668710d624f55bd2e4ed2ea34e76146efa981f5
volkancicek <volkanericlice@gmail.com>
2019-07-06 12:56:18 +0200

    [NO-ISSUE] config change, cermine usage is made optional

--- config.py
+++ config.py
@@ -1,14 +1,26 @@
-conf = {'input_file': "sample.json",
-        'output_file_json': 'reference_strings.json',
-        'output_file_txt': 'reference_strings.txt',
-        'output_file_csv': 'reference_strings.csv',
-        'reference_paper_count': 10,
+input_file = 'input_file'
+output_file_json = 'output_file_json'
+output_file_txt = 'output_file_txt'
+output_file_csv = 'output_file_csv'
+reference_paper_count = 'reference_paper_count'
+reference_styles = 'reference_styles'
+max_reference_per_paper = 'max_reference_per_paper'
+cermine_active = 'cermine_active'
+cermine_result = 'cermine_result'
+
+config = {'input_file': "sample.json",
+        'output_file_json': 'data/reference_strings.json',
+        'output_file_txt': 'data/reference_strings.txt',
+        'output_file_csv': 'data/reference_strings.csv',
+        'reference_paper_count': 6,
         'reference_styles': (
         'apa', 'chicago-annotated-bibliography', 'ieee', 'council-of-science-editors', 'iso690-author-date-en',
         'modern-humanities-research-association', 'modern-language-association',
         'american-society-of-mechanical-engineers', 'american-medical-association',
         'american-chemical-society-author-date', 'american-sociological-association', 'turabian-fullnote-bibliography',
         'acm-sig-proceedings', 'mcgill-en'),
-        'max_reference_per_paper': 3,
-        'cermine_result': 'cermine_result.txt'
+        'max_reference_per_paper': 20,
+        'cermine_active': 0,
+        'cermine_result': 'data/cermine_result.txt'
         }
+
