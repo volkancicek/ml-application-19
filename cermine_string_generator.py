d295db01d2aae06baa998fab2ef3ff06372fc128
dinceralkac <alkacd@gmail.com>
2019-07-19 14:31:14 +0200

    [NO ISSUE] Added some logs for tracebility

--- cermine_string_generator.py
+++ cermine_string_generator.py
@@ -12,6 +12,7 @@ def write_result_cermine_csv(index):
     end_index = len(df)
     for i in range(start_index, end_index):
         try:
+            print('ref_string_count: [' + str(i) + ']')
             bibtex_format = cer.call_cermine(df.iloc[i]['ref_string'])
         except Exception as e:
             print("!!!ERROR calling cermin service for doi: [" + df.iloc[i]['doi'] + "] " + str(e))
@@ -21,14 +22,14 @@ def write_result_cermine_csv(index):
         cermin_list.append(row)
         if start_index % 10 == 0 or start_index == end_index - 1:
             df_bib = pd.DataFrame(cermin_list, columns=['id', 'bibtex_string'])
-            if not os.path.isfile('data/raw/cermine_result.csv'):
+            if not os.path.isfile('data/raw/crossref/cermine_result.csv'):
                 try:
-                    df_bib.to_csv('data/raw/cermine_result.csv', sep='\t', index=None, header=True)
+                    df_bib.to_csv('data/raw/crossref/cermine_result.csv', sep='\t', index=None, header=True)
                 except Exception as e:
                     print("!!!ERROR writing to csv" + str(e))
             else:  # else it exists so append without writing the header
                 try:
-                    df_bib.to_csv('data/raw/cermine_result.csv', sep='\t', mode='a', index=None, header=False)
+                    df_bib.to_csv('data/raw/crossref/cermine_result.csv', sep='\t', mode='a', index=None, header=False)
                 except Exception as e:
                     print("!!!ERROR writing to csv" + str(e))
             cermin_list = list()
@@ -36,4 +37,4 @@ def write_result_cermine_csv(index):
 
 
 if __name__ == '__main__':
-    write_result_cermine_csv(0)
+    write_result_cermine_csv(160)
